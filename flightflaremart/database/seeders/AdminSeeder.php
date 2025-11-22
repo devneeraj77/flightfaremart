6c9d4de92620cd510f9367ba486c687ff2b2d017
devneeraj77 <dev.neerajrekwar@gmail.com>
2025-11-22 19:02:19 +0530

    --add dashboard

---
+++ flightflaremart/database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create default admin
+        Admin::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'admin@flightfaremart.com',
+            'password' => bcrypt('admin123'),
+        ]);
+
+        // Create random admins
+        // Admin::factory(5)->create();
+    }
+}
