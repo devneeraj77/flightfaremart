d6180c8b37ac8cafcc9d872900eb85751101dcc4
devneeraj77 <dev.neerajrekwar@gmail.com>
2026-01-24 17:43:23 +0530

    add .

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -117,7 +117,8 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.828 0l-4.243-4.243a8 8 0 1111.314 0z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                             </svg>
-                            <p class="text-accent dark:text-base-300">703 Waterford Way, Suite 600, Miami, FL 33126.</p>
+                            <p class="text-accent dark:text-base-300">703 Waterford Way, Suite 600,
+Miami, FL 33126.</p>
                         </div>
 
                         <div class="flex items-start space-x-3">
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -83,7 +83,7 @@
       <!-- Contact -->
       <div class="md:w-sm space-x-8 mb-10">
         <h3 class="font-semibold text-black  dark:text-primary pb-2 font-blod text-xl">Contact Us</h3>
-        <p class="text-sm   text-start flex content-center items-center gap-2 py-1">support@flightflaremart.com<a href="mailto:hello@flightfaremart.com"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-external-link">
+        <p class="text-sm   text-start flex content-center items-center gap-2 py-1">support@flightflaremart.com<a href="mailto:support@flightflaremart.com"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-external-link">
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
               <path d="M11 13l9 -9" />
